f20808644d976e0fb79feb05e19b9b18eb69f769
sarwar-asik <shasik2021@gmail.com>
2023-03-09 10:53:12 +0600

    create Object

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ const CarProto = {
   totalProfit() {
     return this.price * this.stock;
   },
+  creator(brand, price, stock) {
+    this.brand = brand;
+    this.price = price;
+    this.stock = stock;
+  },
 };
 
 const m8 = Object.create(CarProto);
-m8.brand = "BMW";
-m8.price = 10;
-m8.stock = 3;
+m8.creator("BMW", 10000, 5);
 console.log(m8, "m8");
+
+const zing = Object.create(CarProto);
+
+zing.creator("Zing", 3000, 8);
+console.log(zing, "zing");
