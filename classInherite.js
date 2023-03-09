f64d3364372626f76fafd82c7228e20db379f387
sarwar-asik <shasik2021@gmail.com>
2023-03-09 11:47:02 +0600

    class Inherit

--- classInherite.js
+++ classInherite.js
@@ -9,11 +9,24 @@ Person.prototype.calculateAge = function (year) {
 };
 
 const asik = new Person("asik", 1999);
-// console.log(asik.calculateAge(2023));
+console.log("age", asik.calculateAge(2023));
 
 const Student = function (name, birthYear, subject) {
-  (this.name = name), (this.birthYear = birthYear), (this.subject = subject);
+  //   (this.name = name), (this.birthYear = birthYear), (this.subject = subject);
+  Person.call(this, name, birthYear);
+  this.subject = subject;
+};
+
+// create a prototype form call Person in student
+Student.prototype = Object.create(Person.prototype);
+
+// /** */ create Prototype //*** */
+Student.prototype.introDuction = function () {
+  return ` I am ${this.name}`;
 };
 
 const mezba = new Student("Mejbah Babu", 1965, "science");
-console.log(mezba);
+console.table(mezba);
+console.log(mezba.introDuction());
+
+console.log("created Proto age", mezba.calculateAge(2003));
