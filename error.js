1576b205b7322e8d06c8b61312b300f79522f784
sarwar-asik <shasik2021@gmail.com>
2023-03-10 10:43:25 +0600

    asynce error

--- error.js
+++ error.js
@@ -1,10 +1,42 @@
 console.log(" -------- error.js -------");
 
+// ** Catch Error with separate
+try {
+  console.lhog("All is well");
+} catch (e) {
+  const totalError = {
+    name: e.name,
+    message: e.message,
+    stack: e.stack,
+  };
 
-try{
-    
-
+  //   console.log("Something went err wrong",totalError)
 }
-catch(e){
 
+try {
+  throw new Error("EEEEEEEEEEE");
+  //   or //
+  //   throw new TypeError("This is TypeError");
+  // or //
+  //   throw new SyntaxError("This is Syntax Error")
+} catch (error) {
+  const totalError = {
+    name: error.name,
+    message: error.message,
+    stack: error.stack,
+  };
+  //   console.log(error, "and", totalError);
 }
+
+(async () => {
+  try {
+    await Promise.reject("Valo are lagena");
+  } catch (error) {
+    const totalError = {
+      name: error.name,
+      message: error.message,
+      stack: error.stack,
+    };
+    console.log(error, "and", totalError);
+  }
+})();
